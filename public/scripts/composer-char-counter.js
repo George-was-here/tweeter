1458d9ef827f35e74a80cf7ce1423648243149ca
George-was-here <george.burt@gmail.com>
2022-04-28 08:20:24 +0000

    Added element to bottom of the page to display tweets and began styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,18 +2,29 @@ console.log("-------------COMPOSER-CHAR-COUNTER TEST");
 
 $(document).ready(function() {
   $('textarea').on("input", function() {
-    var maxlength = $(this).attr("maxlength");
-    var currentLength = $(this).val().length;
+    let maxlength = $(this).attr("maxlength");
+    let currentLength = $(this).val().length;
 
     //if state for red text adding
-    if (currentLength >= maxlength) {
-      return console.log("You have exceeded 140 characters.");
-    }
-    
+    // if (currentLength >= maxlength) {
+    //   console.log("You have exceeded 140 characters.");
+    // }
 
+    // if ($('#counterIcon').val(currentLength > 0)) {
+    //   console.log("HEY GUYS!!!!")
+    //   $('#counterIcon').removeClass("red-text");
+    // }
+  // } else if ($('#counterIcon').val(currentLength < 0)) {
+  //     $('#counterIcon').addClass("red-text");
+  // }
+  
+    // $('#counterIcon').toggleClass("red-text", $('#counterIcon').val(currentLength > 0)); 
     //if statement for red text removing
     //setting new value for counter below.
-    $('#counterIcon').val(maxlength - currentLength);
+    $('#counterIcon').val(140 - currentLength);
     console.log(maxlength - currentLength + " chars left");
+
+
+
   });
 });

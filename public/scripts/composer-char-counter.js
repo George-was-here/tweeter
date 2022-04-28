d91edc1debab9fb7fc7aa3d92359b458168b5291
George-was-here <george.burt@gmail.com>
2022-04-28 19:15:03 +0000

    toggleClass implemented to alternate red text on counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,27 +4,11 @@ $(document).ready(function() {
   $('textarea').on("input", function() {
     let maxlength = $(this).attr("maxlength");
     let currentLength = $(this).val().length;
-
-    //if state for red text adding
-    // if (currentLength >= maxlength) {
-    //   console.log("You have exceeded 140 characters.");
-    // }
-
-    // if ($('#counterIcon').val(currentLength > 0)) {
-    //   console.log("HEY GUYS!!!!")
-    //   $('#counterIcon').removeClass("red-text");
-    // }
-  // } else if ($('#counterIcon').val(currentLength < 0)) {
-  //     $('#counterIcon').addClass("red-text");
-  // }
-  
-    // $('#counterIcon').toggleClass("red-text", $('#counterIcon').val(currentLength > 0)); 
     //if statement for red text removing
+    $('#counterIcon').toggleClass("red-text", (140 - currentLength) < 0);
     //setting new value for counter below.
     $('#counterIcon').val(140 - currentLength);
     console.log(maxlength - currentLength + " chars left");
-
-
 
   });
 });

5236ad6890cb2ed35a762f1d5e2487a146ab8494
George-was-here <george.burt@gmail.com>
2022-04-28 00:59:55 +0000

    Counter working

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,11 +4,16 @@ $(document).ready(function() {
   $('textarea').on("input", function() {
     var maxlength = $(this).attr("maxlength");
     var currentLength = $(this).val().length;
-  
+
+    //if state for red text adding
     if (currentLength >= maxlength) {
       return console.log("You have exceeded 140 characters.");
     }
-  
+    
+
+    //if statement for red text removing
+    //setting new value for counter below.
+    $('#counterIcon').val(maxlength - currentLength);
     console.log(maxlength - currentLength + " chars left");
   });
 });
